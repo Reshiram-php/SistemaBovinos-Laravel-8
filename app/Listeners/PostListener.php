05de40a54bcc1ab8181c3a4d2c24d2c0c14b7387
Kevin Pacheco <david3797@hotmail.com>
2023-02-04 10:29:52 -0500

    final notificaciones queue

--- app/Listeners/PostListener.php
+++ app/Listeners/PostListener.php
@@ -8,7 +8,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Notification;
 
 
-class PostListener
+class PostListener implements ShouldQueue
 {
     /**
      * Create the event listener.
